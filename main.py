b8f8996ba254b8f0d0083de4f5044e2bbdab5554
Kerckk <122673648+Kerckk@users.noreply.github.com>
2023-02-25 11:49:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+class Tomato:
+
+    states = {0: 'отсутствует', 1: 'Цветение', 2: 'Зеленый', 3: 'Красный'}
+
+    def __init__(self, index):
+        self._index = index
+        self._state = 0
+
+    def grow(self):
+        self._change_state()
+
+    def is_ripe(self):
+        if self._state == 3:
+            return True
+        return False
+
+    def _change_state(self):
+        if self._state < 3:
+            self._state += 1
+        self._print_state()
+
+    def _print_state(self):
+        print(f'Помидор {self._index} на стадии {Tomato.states[self._state]}')
+
+
+class TomatoBush:
+
+    def __init__(self, num):
+        self.tomatoes = [Tomato(index) for index in range(0, num)]
+
+    def grow_all(self):
+        for tomato in self.tomatoes:
+            tomato.grow()
+
+    def all_are_ripe(self):
+        return all([tomato.is_ripe() for tomato in self.tomatoes])
+
+    def give_away_all(self):
+        self.tomatoes = []
+
+
+class Gardener:
+
+    def __init__(self, name, plant):
+        self.name = name
+        self._plant = plant
+
+    def work(self):
+        print('Садовник работает...')
+        self._plant.grow_all()
+        print('Садовник завершил')
+
+    def harvest(self):
+        print('Садовник собирает урожай...')
+        if self._plant.all_are_ripe():
+            self._plant.give_away_all()
+            print('Сбор урожая завершен')
+        else:
+            print('Слишком рано! Ваше растение зеленое и не созрело')
+
+    @staticmethod
+    def knowledge_base():
+        print('''Harvest time for tomatoes should ideally occur
+when the fruit is a mature green and
+then allowed to ripen off the vine.
+This prevents splitting or bruising
+and allows for a measure of control over the ripening process.''')
+
+
+if __name__ == '__main__':
+    Gardener.knowledge_base()
+    great_tomato_bush = TomatoBush(4)
+    gardener = Gardener('Emilio', great_tomato_bush)
+    gardener.work()
+    gardener.work()
+    gardener.harvest()
+    gardener.work()
+    gardener.harvest()
+
